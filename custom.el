a8e789e7d3e12b255bca8d429ef595cf66eaffb1
Alex Covington <alex@covington.tech>
2022-11-14 00:25:18 -0800

    Automatically update ToC

--- custom.el
+++ custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(helm-minibuffer-history-key "M-p")
  '(package-selected-packages
-   '(magit org-bullets treemacs-evil treemacs helm-lsp lsp-ui lsp-mode which-key company-lsp company slime ace-window helm general evil-leader evil-collection monokai-theme use-package)))
+   '(toc-org magit org-bullets treemacs-evil treemacs helm-lsp lsp-ui lsp-mode which-key company-lsp company slime ace-window helm general evil-leader evil-collection monokai-theme use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
