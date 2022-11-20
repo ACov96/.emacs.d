4d01a10dd2348a6f072d5d5061f5ba887da3ecb3
Alex Covington <alex@covington.tech>
2022-11-19 19:16:29 -0800

    Add emoji support

--- custom.el
+++ custom.el
@@ -8,7 +8,7 @@
    '("bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "fee7287586b17efbfda432f05539b58e86e059e78006ce9237b8732fde991b4c" "78e6be576f4a526d212d5f9a8798e5706990216e9be10174e3f3b015b8662e27" "4c56af497ddf0e30f65a7232a8ee21b3d62a8c332c6b268c81e9ea99b11da0d3" "6ca5f925de5c119694dbe47e2bc95f8bad16b46d154b3e2e0ae246fec4100ec5" default))
  '(helm-minibuffer-history-key "M-p")
  '(package-selected-packages
-   '(powerline color-theme-sanityinc-tomorrow olivetti nova-theme projectile all-the-icons dashboard org-superstar org-appear mixed-pitch company-posframe beacon company-box evil toc-org magit org-bullets treemacs-evil treemacs helm-lsp lsp-ui lsp-mode which-key company-lsp company slime ace-window helm general evil-leader evil-collection monokai-theme use-package)))
+   '(emojify powerline color-theme-sanityinc-tomorrow olivetti nova-theme projectile all-the-icons dashboard org-superstar org-appear mixed-pitch company-posframe beacon company-box evil toc-org magit org-bullets treemacs-evil treemacs helm-lsp lsp-ui lsp-mode which-key company-lsp company slime ace-window helm general evil-leader evil-collection monokai-theme use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
